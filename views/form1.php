ac22a849835aea83a98cbab3f92e520d577e2fa9
kwikl3arn <44991804+kwikl3arn@users.noreply.github.com>
2019-09-25 09:27:50 +0530

    Create form1.php

---
+++ views/form1.php
@@ -0,0 +1,9 @@
+<html>
+<body>
+<form action="<?php echo base_url()?>mycontroller/form1_submit" type="post">
+<input type="text" name="username" id="username">
+<input type="text" name="skills" id="skills">
+<input type="submit">
+</form>
+</body>
+</html>
