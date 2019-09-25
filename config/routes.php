5f00a01a9a46baaa0b969e05625ca0f7f82d8d0e
kwikl3arn <44991804+kwikl3arn@users.noreply.github.com>
2019-09-25 09:59:30 +0530

    Create routes.php

---
+++ config/routes.php
@@ -0,0 +1,4 @@
+$route['default_controller'] = 'Mycontroller/index';
+$route['Application_1']='Mycontroller/form1';
+$route['Application_2']='Mycontroller/form2';
+$route['Application_3']='Mycontroller/form3';
