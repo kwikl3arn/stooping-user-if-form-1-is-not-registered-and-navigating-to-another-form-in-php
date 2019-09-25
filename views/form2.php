d3977377a5de5150f7c45b60bf63cf58e3144ad0
kwikl3arn <44991804+kwikl3arn@users.noreply.github.com>
2019-09-25 09:29:20 +0530

    Create form2.php

---
+++ views/form2.php
@@ -0,0 +1,9 @@
+<html>
+<body>
+<form action="<?php echo base_url()?>mycontroller/form2_submit" type="post">
+<input type="text" name="project_name" id="project_name">
+<input type="text" name="project_info" id="project_info">
+<input type="submit">
+</form>
+</body>
+</html>
